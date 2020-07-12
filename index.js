30646a3d93939ec3c8c68f9a8255b1dc831d49e2
Michael Martinez <michaelmartinez@Michaels-Air.fios-router.home>
2020-07-11 22:26:47 -0400

    Headers2

--- index.js
+++ index.js
@@ -19,16 +19,15 @@ const ngrok = process.env.NGROK_ENABLED === 'true' ? require('ngrok') : null;
 //const queryString = require('query-string');
 
 
-app.use(express.static(path.join(__dirname, '/public')));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use(function(req, res, next) {
+app.use(cors(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
-});
-
+}));
+app.use(express.static(path.join(__dirname, '/public')));
 //serve static assets if in production
 /*
 if (process.env.NODE_ENV === "production") {
